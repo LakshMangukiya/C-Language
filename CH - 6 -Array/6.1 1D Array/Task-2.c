916873207a718970800b6ec3739ba4bd8d6f2979
Laksh Mangukiya <mangukiyalaksh7@gmail.com>
2024-08-01 17:40:56 +0530

    Add files via upload

---
+++ CH - 6 -Array/6.1 1D Array/Task-2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define p printf
+#define s scanf
+
+int main()
+{
+    int n,i,sum=0;
+    p("Enter size of array : ");
+    s("%d",&n);
+
+    int a[n];
+
+    for (i = 0; i < n; i++)
+    {
+        p("a[%d] : ",i);
+        s("%d",&a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum+=a[n];
+    }
+
+    p("Average of array : %.2f",(float)sum/n);
+    
+}
---
+++ CH - 6 -Array/6.2 2D Array/TASK--2.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define p printf
+#define s scanf
+
+int main()
+{
+    int n1,n2;
+    p("Enter first year : ");
+    s("%d",&n1);
+    p("Enter second year : ");
+    s("%d",&n2);
+
+    int n=0,a[n2-n1];
+        for (int j = n1; j <= n2; j++)
+        {
+            if (j%4==0)
+            {
+                a[n] = j;
+                n++;
+            } 
+        }
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d\n",a[i]);
+        }
+}
